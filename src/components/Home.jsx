9f4df22dc13329364e53903ed3acf0145fb14a30
Sara <saritapz01@hotmail.com>
2022-02-22 13:40:03 -0500

    Funciona

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,12 +16,12 @@ export default class Home extends Component {
                         <Link to="/RetoFinal-Sprint2/questionHTML" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-html_nlmuza.png" rounded width="100px"/> </Link>
                     </div>
                     <div>
-                        <Link to="../questionCSS" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-css_ns2ytm.png" rounded width="100px" style={{ marginRight: "25px" , marginTop: "25px" }} /> </Link>
-                        <Link to="../questionJS" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-js_clmjzv.png" rounded width="100px" /> </Link>
+                        <Link to="/RetoFinal-Sprint2/questionCSS" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-css_ns2ytm.png" rounded width="100px" style={{ marginRight: "25px" , marginTop: "25px" }} /> </Link>
+                        <Link to="/RetoFinal-Sprint2/questionJS" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-js_clmjzv.png" rounded width="100px" /> </Link>
                     </div>
                     <div>
-                        <Link to="../questionFIGMA" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-figma_djaqpd.png" rounded width="100px" style={{ marginRight: "25px" , marginTop: "25px"}} /> </Link>
-                        <Link to="../questionUX" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-ux_gui8ri.png" rounded width="100px" /> </Link>
+                        <Link to="/RetoFinal-Sprint2//questionFIGMA" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-figma_djaqpd.png" rounded width="100px" style={{ marginRight: "25px" , marginTop: "25px"}} /> </Link>
+                        <Link to="/RetoFinal-Sprint2/questionUX" className="nav-link"> <Image src="https://res.cloudinary.com/sarapalacio01/image/upload/v1645318304/Quiz-Reto2/Categoria-ux_gui8ri.png" rounded width="100px" /> </Link>
                     </div>
                     <DivFooter>
                         <NavBar />
